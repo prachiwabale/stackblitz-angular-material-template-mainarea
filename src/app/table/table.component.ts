10d1a669cd93846247b97dda53cf0dc9b810e9f9
prachiwabale <prachiwabale2091993@gmail.com>
2022-04-13 14:00:35 +0530

    2 files modified

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -7,8 +7,20 @@ export interface PeriodicElement {
   symbol: string;
 }
 
+export interface DataElememt {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 const ELEMENT_DATA: PeriodicElement[] = [
   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+];
+
+const TABLE2DATA: DataElememt[] = [
+
   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
 ];
@@ -21,7 +33,7 @@ export class TableComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   displayedColumns1: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
-  dataSource = ELEMENT_DATA;
+  dataSource1 = TABLE2DATA;
   constructor() {}
 
   ngOnInit() {}
